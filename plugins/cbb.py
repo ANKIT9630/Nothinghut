8cb6659c32941a01dae7034b1c39332e546b29be
ANKIT9630 <119794402+ANKIT9630@users.noreply.github.com>
2023-05-13 21:53:56 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>∞۝Aⲛⲕⲓⲧ۝∞</a>\n○ Creator : <a href='https://t.me/Saurav3BV6SA9LLElon7Musk'>꧁༒༺☠︎☬ S҉₳𝖀𝕽₳V҉☠︎☬༒꧂</a>\n○ Channel: @defenderofthemultiverse Group : @thewarriorsreal</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
